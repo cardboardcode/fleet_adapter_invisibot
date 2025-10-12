584af200bdcc17589dbad282ae82c16db6fcc8b4
Gary Bey <beyhy94@gmail.com>
2025-10-12 22:48:21 +0800

    :hammer: Implemented running with use of ros2 launch instead of ros2 run.

--- scripts/2_build_docker_container.bash
+++ scripts/2_build_docker_container.bash
@@ -1,14 +1,20 @@
 #!/usr/bin/env bash
 
+CONFIG_FILE="/fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/config.yaml"
+NAV_GRAPH_FILE="/fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/nav_graph.yaml"
+TRAJECTORY_SERVER_URL="ws://localhost:8000/_internal"
+
 docker run -it --rm \
 	--name fleet_adapter_invisibot_c \
 	--network host \
 	-e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
-	-v ./fleet_adapter_invisibot/config.yaml:/fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/config.yaml \
-    -v ./fleet_adapter_invisibot/nav_graph.yaml:/fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/nav_graph.yaml \
+	-v ./fleet_adapter_invisibot/config.yaml:$CONFIG_FILE \
+    -v ./fleet_adapter_invisibot/nav_graph.yaml:$NAV_GRAPH_FILE \
 fleet_adapter_invisibot:jazzy bash -c \
 "source /ros_entrypoint.sh && \
-ros2 run fleet_adapter_invisibot fleet_adapter \
--c /fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/config.yaml \
--n /fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/nav_graph.yaml \
--s ws://localhost:8000/_internal"
+ros2 launch fleet_adapter_invisibot run.launch.xml \
+config_file:=$CONFIG_FILE \
+nav_graph_file:=$NAV_GRAPH_FILE \
+server_uri:=$TRAJECTORY_SERVER_URL"
+
+unset CONFIG_FILE NAV_GRAPH_FILE
