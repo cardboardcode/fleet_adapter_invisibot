4cb88b8e7acbfa239379b79a3ca95cd6a2ecdbae
Gary Bey <beyhy94@gmail.com>
2026-06-03 21:36:34 +0800

    :heavy_plus_sign: Added dev script to quickly deploy docker container.
    Signed-off-by: Gary Bey <beyhy94@gmail.com>

---
+++ scripts/3_deploy_docker_container.bash
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+CONFIG_FILE="/fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/config.yaml"
+NAV_GRAPH_FILE="/fleet_adapter_invisibot_ws/src/fleet_adapter_invisibot/nav_graph.yaml"
+TRAJECTORY_SERVER_URL="ws://localhost:8000/_internal"
+
+docker run -it -d \
+	--name fleet_adapter_invisibot_c \
+	--restart unless-stopped \
+	--network host \
+	-e RCL_LOG_LEVEL=debug \
+	-e RCUTILS_COLORIZED_OUTPUT=1 \
+	-e RMW_IMPLEMENTATION=rmw_cyclonedds_cpp \
+	-v ./fleet_adapter_invisibot/config.yaml:$CONFIG_FILE \
+    -v ./fleet_adapter_invisibot/nav_graph.yaml:$NAV_GRAPH_FILE \
+fleet_adapter_invisibot:jazzy bash -c \
+"source /ros_entrypoint.sh && \
+ros2 launch fleet_adapter_invisibot run.launch.xml \
+config_file:=$CONFIG_FILE \
+nav_graph_file:=$NAV_GRAPH_FILE \
+server_uri:=$TRAJECTORY_SERVER_URL"
+
+unset CONFIG_FILE NAV_GRAPH_FILE
